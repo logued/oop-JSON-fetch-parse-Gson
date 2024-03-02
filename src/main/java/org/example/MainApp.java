c3245865b15d15d06fec37dc97ae8b8612800bff
dermo <dermot.logue@dkit.ie>
2024-03-02 20:49:53 +0000

    Added som etry-catch blocks for error handling

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,17 +1,19 @@
 package org.example;                // March 2024
 
 import com.google.gson.Gson;
-
-import java.io.IOException;
+import com.google.gson.JsonSyntaxException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 /**
- * Demonstrates how to fetch JSON from a remote API (Endpoint), and
- * parse the returned JSON string into a Java Object that has
- * a corresponding structure.
+ * Demonstrates:
+ * - Use a HttpClient to get response from a remote API (Endpoint)
+ * - ( i.e. http://api.open-notify.org/iss-now.json )
+ * - get the JSON string (the response body from the response)
+ * - convert the JSON string into a Java object using the Gson Parser
+ * - check the return status code from the Http response
  * <p>
  * This sample uses the following API:
  * API Request:   http://api.open-notify.org/iss-now.json
@@ -29,20 +31,37 @@ import java.net.http.HttpResponse;
 
 class MainApp {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    final String URL = "http://api.open-notify.org/iss-now.json";   // address of API Endpoint
 
-        final String url = "http://api.open-notify.org/iss-now.json";
+    public static void main(String[] args)  {
+        MainApp app = new MainApp();
+        app.start();
+    }
 
+    public void start() {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
+        HttpRequest request = HttpRequest.newBuilder()  // build an HTTP request
+                .uri(URI.create(URL))
                 .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = null;
+        // send() throws a Checked Exceptions, so we need to provide a try-catch block
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch(Exception e){
+            e.printStackTrace();
+        }
 
-        String jsonString = response.body();
+        // Check the response code returned from the API endpoint
+        // A code of 200 indicates success
+        if( response.statusCode() != 200 ) {
+            System.out.println("HTTP Request failed - Status code returned = " + response.statusCode());
+            return;
+        }
+        // get the body (the data payload) from the HTTP response
+        String jsonResponseString = response.body();
 
-        if (jsonString == null) {
+        if (jsonResponseString==null) {
             System.out.println("Json String was empty.");
             return;
         }
@@ -50,13 +69,17 @@ class MainApp {
         // Instantiate (create) a Gson Parser
         Gson gsonParser = new Gson();
 
-        // Call the fromJson() method in the parser to create a new ISSPositionTime object
-        // and populate it with the JSON string data.
+        // Call the fromJson() method of the parser to create a new ISSPositionTime object
+        // and populate it with the JSON String data.
         // The class field names must match the key names in the json string.
-
-        IssPositionAtTime issPositionAtTime = gsonParser.fromJson(jsonString, IssPositionAtTime.class);
-
+        IssPositionAtTime issPositionAtTime=null;
+        try {
+            issPositionAtTime = gsonParser.fromJson(jsonResponseString, IssPositionAtTime.class);
+        }
+        catch( JsonSyntaxException ex)
+        {
+            System.out.println("Jason syntax error encountered. " + ex);
+        }
         System.out.println(issPositionAtTime);
     }
-
 }
